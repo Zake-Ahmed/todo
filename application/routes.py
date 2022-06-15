540ea5bc3e4b2edaac3e785b24cf0c90bf8f9929
Zake-Ahmed <zake3012@gmail.com>
2022-06-15 13:59:56 +0000

    final change

--- application/routes.py
+++ application/routes.py
@@ -4,4 +4,35 @@ from application.models import Todos
 
 @app.route('/add')
 def add():
+
+    
     return  'Added a new todo'
+
+@app.route('/add/taskname/<task>')
+def taskname(task):
+    todo = Todos(task =task,completed = False)
+    db.session.add(todo)
+    db.session.commit()
+    return task
+
+@app.route('/complete/<task>')
+def complete(task):
+    todo = Todos.query.filter_by(task=task).first()
+    todo.completed=True
+    db.session.commit()
+    return str(todo.completed)
+
+@app.route('/delete/<task>')
+def delete(task):
+    todo = Todos.query.filter_by(task=task).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return f'Deleted {task}'
+
+
+@app.route('/update/<oldtask>/<newtask>')
+def update(oldtask,newtask):
+    todo = Todos.query.filter_by(task=oldtask).first()
+    todo.task= newtask
+    db.session.commit()
+    return newtask
